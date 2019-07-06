c27ae273dd0ed9b1da533436e1e187d472bbf333
Lewis Williams <LewisWilliams@abcdeMacBook.local>
2019-07-06 11:43:02 +0100

    login/signup

---
+++ functions/helpers/index.js
@@ -0,0 +1,9 @@
+const isEmpty = email => email.trim() === '';
+
+const  isEmail = email => {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return email.match(re);
+};
+
+module.exports.isEmpty = isEmpty;
+module.exports.isEmail = isEmail;
